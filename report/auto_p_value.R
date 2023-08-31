dba9d25d1653bab5662978e968a82c0354670f7c
Jota Saraiva <jpsaraiva2002@gmail.com>
2023-08-31 14:21:11 -0300

    Ajusta script de p-valores

--- report/auto_p_value.R
+++ report/auto_p_value.R
@@ -7,20 +7,30 @@ options(scipen = 999)
 
 load(here("data","tabela_total.rda"))
 
-i <- "mortos_por_pop"
+df_total <- df_total |> select(-mortos_por_pop, -ano)
 
-for (i in df_total |> colnames()) {
-  rc_temp <- df_total |> 
+p_valores <- list()
+
+modelo <- linear_reg() |> set_engine("lm")
+
+for (i in colnames(df_total)) {
+  rc_temp <- df_total |>
     recipe(mortes ~ .) |>
-    step_naomit(all_numeric()) |> 
-    step_normalize(all_numeric_predictors()) |>
-    step_select(mortes, i)
-  
-  modelo <- linear_reg() |> set_engine("lm")
-  
-  wflow_fit <- workflow() |> 
-    add_recipe()
+    step_naomit(all_numeric()) |>
+    step_select("mortes",i)
+
+  wflow_fit <- workflow() |>
+    add_recipe(rc_temp) |>
+    add_model(modelo) |>
+    fit(df_total)
+
+  if (i == "ano") {
+    p_valores <- wflow_fit |> tidy()
+  } else {
+    p_valores <- rbind(p_valores, wflow_fit |> tidy())
+  }
 }
 
+p_valores <- filter(p_valores, term != "(Intercept)") |> arrange(p.value)
 
-
+menor_pvalor <- filter(p_valores, p.value == min(p.value))
