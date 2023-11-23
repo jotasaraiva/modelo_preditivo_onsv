e5066431457292192324063cf4417a6e895e582a
Jota Saraiva <jpsaraiva2002@gmail.com>
2023-11-23 16:15:38 -0300

    Adiciona scripts para coleta de dados de frota

---
+++ data-raw/frota_mensal.R
@@ -0,0 +1,187 @@
+library(tidyverse)
+library(here)
+
+read_fleet <- function(file) {
+  
+  ext <- tools::file_ext(file)
+  
+  if (ext == "zip") {
+    
+    temp <- tempfile()
+    
+    download.file(file, destfile = temp)
+    
+    file_name <- unzip(temp, list = T)$Name
+    
+    unzip(temp, exdir = tempdir())
+    
+    data <- readxl::read_excel(
+      paste0(tempdir(),"/",file_name),
+      sheet = 2,
+      skip = 3
+    )
+    
+    unlink(paste0(tempdir(),"/",file_name))
+    unlink(temp)
+    
+    return(data)
+  } 
+  
+  else if (ext == "xlsx") {
+    
+    tryCatch(
+      expr = {
+        name <- "tempfile.xlsx"
+        
+        download.file(file, destfile = name, mode = "wb")
+        
+        data <- readxl::read_excel(name, sheet = 2, skip = 3)  
+        
+        unlink(name)
+        
+        return(data)
+      },
+      error = function(e) {
+        name <- "tempfile.xlsx"
+        
+        download.file(file, destfile = name, mode = "wb")
+        
+        data <- readxl::read_excel(name, sheet = 1, skip = 3)  
+        
+        unlink(name)
+        
+        return(data)
+      }
+    )
+    
+  }
+  
+  else if (ext == "xls") {
+    
+    name <- "tempfile.xls"
+    
+    download.file(file, destfile = name, mode = "wb")
+    
+    data <- readxl::read_excel(name, sheet = 1, skip = 2)
+    
+    unlink(name)
+    
+    return(data)
+  }
+  
+  else { 
+    data <- 0
+    return(data)
+  }
+}
+
+fleet_links <- function(url) {
+  page <- read_html(url)
+  
+  links <- page |>  
+    html_elements("a") |> 
+    html_attr("href")
+  
+  link_texts <- page |>  
+    html_elements("a") |> 
+    html_text2()
+  
+  ids <- link_texts |> 
+    tolower() |> 
+    str_detect("por (município|municipio)") |> 
+    which(TRUE)
+  
+  files <- links[ids]
+  
+  return(files)
+}
+
+read_fleet_page <- function(page) {
+  links <- fleet_links(page)
+  
+  frota <- lapply(links, read_fleet)
+  
+  for (i in seq(length(frota),1,-1)) {
+    frota[[i]]$MES <- i
+    frota[[i]]$ANO <- as.numeric(str_extract(page_list[num], "(1|2)\\d{3}"))
+  }
+  
+  frota <- lapply(frota, select, c(UF,TOTAL,AUTOMOVEL,CAMINHONETE,
+                                   CAMIONETA,UTILITARIO,MOTOCICLETA,
+                                   CICLOMOTOR,MOTONETA,MES,ANO))
+  frota <- lapply(
+    frota,
+    mutate,
+    across(!UF, as.numeric)
+  )
+  
+  frota <- reduce(frota, full_join)
+  
+  return(frota)
+}
+
+read_fleet2011 <- function() {
+  
+  folder <- here("data-raw","frota_mensal","frota_2011","Frota Munic. 2011")
+  files <- list.files(folder)
+  paths <- paste(sep = "/", folder, files)
+  files_info <- file.info(paths)
+  files_info <- files_info[with(files_info, order(as.POSIXct(mtime))), ]
+  paths <- rownames(files_info)
+  
+  frota <- lapply(paths, readxl::read_excel, sheet = 2, skip = 2)
+  
+  for(i in 1:length(frota)) {
+    x <- frota[[i]]
+    x <- x[x$UF != "UF", ]
+    x$ANO <- 2011
+    x$MES <- i
+    frota[[i]] <- x 
+  }
+  
+  frota <- lapply(frota, select, c(UF,TOTAL,AUTOMOVEL,CAMINHONETE,
+                                   CAMIONETA,UTILITARIO,MOTOCICLETA,
+                                   CICLOMOTOR,MOTONETA,MES,ANO))
+  frota <- lapply(
+    frota,
+    mutate,
+    across(!UF, as.numeric)
+  )
+  
+  frota <- reduce(frota, full_join)
+  
+  return(frota)
+}
+
+read_fleet2012 <- function() {
+  
+  folder <- here("data-raw","frota_mensal","frota_2012","Frota Munic. 2012")
+  files <- list.files(folder)
+  paths <- paste(sep = "/", folder, files)
+  files_info <- file.info(paths)
+  files_info <- files_info[with(files_info, order(as.POSIXct(mtime))), ]
+  paths <- rownames(files_info)
+  
+  frota <- lapply(paths, readxl::read_excel, sheet = 2, skip = 2)
+  
+  for(i in 1:length(frota)) {
+    x <- frota[[i]]
+    x <- x[x$UF != "UF", ]
+    x$ANO <- 2012
+    x$MES <- i
+    frota[[i]] <- x 
+  }
+  
+  frota <- lapply(frota, select, c(UF,TOTAL,AUTOMOVEL,CAMINHONETE,
+                                   CAMIONETA,UTILITARIO,MOTOCICLETA,
+                                   CICLOMOTOR,MOTONETA,MES,ANO))
+  frota <- lapply(
+    frota,
+    mutate,
+    across(!UF, as.numeric)
+  )
+  
+  frota <- reduce(frota, full_join)
+  
+  return(frota)
+}
