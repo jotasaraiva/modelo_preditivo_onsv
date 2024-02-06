fe604b69735e3b4663ef92309a0ebfee61d1cdf6
Jota Saraiva <jpsaraiva2002@gmail.com>
2024-02-06 11:34:30 -0300

    Transforma modelo linear anual em script

---
+++ R/modelo_linear_anual.R
@@ -0,0 +1,47 @@
+
+library(dplyr)
+
+load("data/tabela_total.rda")
+
+linear_anual_recipe <- function(data) {
+  rec <- data |> 
+    recipe(
+      mortes ~ 
+        veiculos_total + 
+        qnt_acidentes_fatais +
+        condutores + 
+        qnt_acidentes
+    ) |> 
+    step_normalize(all_numeric_predictors())
+  
+  return(rec)
+}
+
+linear_anual_wflow <- function(recipe, data) {
+  linear_specs <- 
+    linear_reg() |> 
+    set_engine("lm")
+  
+  linear_wflow <-
+    workflow() |> 
+    add_model(linear_specs) |> 
+    add_recipe(recipe) |> 
+    fit(data)
+  
+  return(linear_wflow)  
+}
+
+df_preprocess <- df_total |> 
+  select(
+    ano,
+    mortes,
+    veiculos_total,
+    qnt_acidentes_fatais,
+    condutores,
+    qnt_acidentes,
+    populacao
+  ) |> 
+  drop_na()
+
+modelo <- linear_anual_wflow(recipe = linear_anual_recipe(df_preprocess), data = df_preprocess)
+
